3c3a4a0573574e11ec10b988667ecc0b29c84d0b
warinww <warinrinrin14@gmail.com>
2025-07-24 13:53:24 +0700

    graph

--- lib/widgets/today_hora_chart_widget.dart
+++ lib/widgets/today_hora_chart_widget.dart
@@ -9,7 +9,7 @@ class TodayHoraChart extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(8.0),
+      padding: const EdgeInsets.all(10),
       child: BarChart(
         BarChartData(
           alignment: BarChartAlignment.spaceAround,
@@ -17,26 +17,24 @@ class TodayHoraChart extends StatelessWidget {
           borderData: FlBorderData(show: false),
           titlesData: FlTitlesData(
             topTitles: const AxisTitles(
-              sideTitles: SideTitles(
-                showTitles: false,
-              ),
+              sideTitles: SideTitles(showTitles: false),
             ),
             leftTitles: const AxisTitles(
-              sideTitles: SideTitles(
-                showTitles: false,
-              ),
+              sideTitles: SideTitles(showTitles: false),
             ),
             rightTitles: const AxisTitles(
-              sideTitles: SideTitles(
-                showTitles: false,
-              ),
+              sideTitles: SideTitles(showTitles: false),
             ),
             bottomTitles: AxisTitles(
               sideTitles: SideTitles(
                 showTitles: true,
-                getTitlesWidget: (value, meta) => Text(
-                  'ย.${value.toInt() + 1}',
-                  style: Theme.of(context).textTheme.bodySmall,
+                reservedSize: 30,
+                getTitlesWidget: (value, meta) => Padding(
+                  padding: const EdgeInsets.only(top: 8.0),
+                  child: Text(
+                    'ย.${value.toInt() + 1}',
+                    style: Theme.of(context).textTheme.bodySmall,
+                  ),
                 ),
               ),
             ),
@@ -46,24 +44,61 @@ class TodayHoraChart extends StatelessWidget {
     );
   }
 
+List<BarChartRodStackItem> generateStackItemsWithGradient(
+    double from, double to, Color startColor, Color endColor) {
+  List<BarChartRodStackItem> items = [];
+
+  int steps = 100;
+  if (steps < 1 || from == to) return items;
+
+  double stepSize = (to - from) / steps;
+
+  for (int i = 0; i < steps; i++) {
+    double start = from + i * stepSize;
+    double end = start + stepSize;
+    Color color = Color.lerp(startColor, endColor, i / (steps - 1))!;
+    items.add(BarChartRodStackItem(start, end, color));
+  }
+
+  return items;
+}
+
+
+
   List<BarChartGroupData> createBarGroups() {
     return List.generate(h.length, (index) {
       double topValue = h[index][0].toDouble();
       double bottomValue = h[index][1].toDouble();
 
+      List<BarChartRodStackItem> stackItems = [];
+
+      if (bottomValue < 0) {
+        stackItems.addAll(generateStackItemsWithGradient(
+          bottomValue,
+          0,
+          const Color(0xFF862D2D),
+          const Color(0xFFcbbcbc),
+        ));
+      }
+
+      if (topValue > 0) {
+        stackItems.addAll(generateStackItemsWithGradient(
+          0,
+          topValue,
+          const Color(0xFFbec6c1),
+          const Color(0xFF316141),
+        ));
+      }
+
       return BarChartGroupData(
         x: index,
         barRods: [
           BarChartRodData(
-            toY: topValue,
-            fromY: bottomValue,
-            width: 16,
-            borderRadius: BorderRadius.zero,
-            rodStackItems: [
-              if (topValue > 0) BarChartRodStackItem(0, topValue, Colors.green),
-              if (bottomValue < 0)
-                BarChartRodStackItem(bottomValue, 0, Colors.red),
-            ],
+            fromY: bottomValue < 0 ? bottomValue : 0,
+            toY: topValue > 0 ? topValue : 0,
+            width: 12,
+            rodStackItems: stackItems,
+            borderRadius: BorderRadius.circular(20),
           ),
         ],
       );
